588fd56e88920d0bbb8f373b679da2e6324af9c6
Fabiano Vasconcelos <fvasconcelos@gmail.com>
2023-10-06 09:51:47 -0300

    Versão estável. Já cospe um CPF sem o DV, mas também sem verificação.

--- cpf.cpp
+++ cpf.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::setw;
+using std::setfill;
+using std::string;
 
 int main() {
-    std::cout << "Quantas possibilidades existem para o CPF 245.XXX.XX3-97?" << std::endl;
+    cout << "Quantas possibilidades existem para o CPF 245.XXX.XX3-97?" << endl;
+
+    int cpf_frag = 3;
+    int i;
+
+    for (i = 0; cpf_frag <= 999993; i++) {
+        cout << "Possibilidade " << setw(5) << setfill('0') << i << ": 245" << setw(6) << setfill('0') << cpf_frag << endl;
+        if (i >= 10) break;
+        cpf_frag += 10;
+    }
+
     return 0;
 }
